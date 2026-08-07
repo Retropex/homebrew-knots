d5a79b68bf5ab5c86bc72e73e891a27f891e618b
Léo Haf <leo@haf.ovh>
2026-08-07 10:59:49 +0200

    Update GUI to v29.4.knots20260508

--- Casks/knots-gui.rb
+++ Casks/knots-gui.rb
@@ -1,11 +1,11 @@
 cask "knots-gui" do
     arch arm: "arm64", intel: "x86_64"
   
-    version "29.3.3"
-    sha256 arm:   "92d34a8ca1d3f4e40b869be340375203cf5fc6e9c6729dbf5a9c923cbe6a733e",
-           intel: "8a145f0a88407bf551bea9c3bbd77bf7c9edd4473c6e2280e716457a8e786bc3"
+    version "29.4"
+    sha256 arm:   "4218c01abf01aad7086dc4a1c332c0ea3e110aacf65dc6bc7e1f35d443725acb",
+           intel: "c054aef2142be9cbb247515bb2cdc302672d7676ab81a5e91cfe8f61271534d1"
   
-    url "https://bitcoinknots.org/files/29.x/29.3.knots20260508/bitcoin-29.3.knots20260508-#{arch}-apple-darwin.zip"
+    url "https://bitcoinknots.org/files/29.x/29.4.knots20260508/bitcoin-29.4.knots20260508-#{arch}-apple-darwin.zip"
     name "Bitcoin Knots"
     desc "Bitcoin node"
     homepage "https://bitcoinknots.org/"
